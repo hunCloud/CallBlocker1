868ddda95ba7116b4747747a570c426385f02ac0
NguyenTrung06061997 <49978091+NguyenTrung06061997@users.noreply.github.com>
2019-10-18 16:40:36 +0700

    fix different path

---
+++ app/src/main/java/hungnn/bognmaiit/callblocker/activity/IncomingCallActivity.java
@@ -0,0 +1,18 @@
+package hungnn.bognmaiit.callblocker.activity;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import hungnn.bognmaiit.callblocker.R;
+
+
+public class IncomingCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_call);
+    }
+}
---
+++ app/src/main/java/hungnn/bognmaiit/callblocker/blockednumber/BlockedNumberDao.java
@@ -0,0 +1,22 @@
+package hungnn.bognmaiit.callblocker.blockednumber;
+
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface BlockedNumberDao {
+    @Query("SELECT * FROM blockednumbers")
+    List<BlockedNumber> getAll();
+
+    @Insert
+    void insert(BlockedNumber number);
+
+    @Delete
+    void delete(BlockedNumber number);
+}
